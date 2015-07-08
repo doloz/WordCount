d84d2a0a7b2271b989b62d5909e822d87bc49967
Alexander Doloz <doloz.alexander@gmail.com>
2015-07-08 15:59:04 +0600

    Initial Commit

---
+++ WordList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordList
+//
+//  Created by Alexander Doloz on 7/8/15.
+//  Copyright (c) 2015 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
